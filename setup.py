ce23d953dd9af8f9ba6eb54ca53485eaf49b4f14
ericbrasiln <profericbrasil@unilab.edu.br>
2021-08-24 21:37:36 -0300

    install_requires

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
     name = 'resumos-anpuh-cli',
     version = '0.1.0',
     packages = ['resumosanpuh'],
-    install_requires = ['bs4', 'pandas', 'requests', 'lxml'],
+    install_requires = ['requirements.txt'],
     entry_points = {
         'console_scripts': [
             'resumosanpuh = resumosanpuh.__main__:main'
